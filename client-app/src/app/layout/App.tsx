126703f8e1dd2d9ab53e7cf081de3fbbd2d2997b
Jason Pham <vinhphuong0206@gmail.com>
2019-11-18 21:11:07 +1000

    finished CRUD on client

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,75 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { Header, Icon, List, Menu, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { IActivity } from '../../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 const App = () => {
-  const [activities, setActivities] = useState<IActivity[]>([]);
+    const [activities, setActivities] = useState<IActivity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+        null
+    );
+    // Toggle Edit form
+    const [editMode, setEditMode] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get<IActivity[]>('http://localhost:5000/api/activities')
-      .then(response => {
-        setActivities(response.data);
-      });
-  }, []); // Second parameters to ensure this function does not get stuck in a loop
+    const handleSelectActivity = (id: string) => {
+        setSelectedActivity(activities.filter(a => a.id === id)[0]);
+    };
 
-  return (
-    <Fragment>
-      <NavBar />
-      <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard activities={activities} />
-      </Container>
-    </Fragment>
-  );
+    const handleOpenCreateForm = () => {
+        setSelectedActivity(null);
+        setEditMode(true);
+    };
+
+    const handleCreateActivity = (activity: IActivity) => {
+        setActivities([...activities, activity]);
+    };
+
+    const handleEditActivity = (activity: IActivity) => {
+        setActivities([
+            ...activities.filter(a => a.id !== activity.id),
+            activity,
+        ]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+    };
+
+    const handleDeleteActivity = (id: string) => {
+        setActivities([...activities.filter(a => a.id !== id)]);
+    };
+
+    useEffect(() => {
+        axios
+            .get<IActivity[]>('http://localhost:5000/api/activities')
+            .then(response => {
+                let activities: IActivity[] = [];
+                response.data.forEach(activity => {
+                    activity.date = activity.date.split('.')[0];
+                    activities.push(activity);
+                });
+                setActivities(activities);
+            });
+    }, []); // Second parameters to ensure this function does not get stuck in a loop
+
+    return (
+        <Fragment>
+            <NavBar openCreateForm={handleOpenCreateForm} />
+            <Container style={{ marginTop: '7em' }}>
+                <ActivityDashboard
+                    activities={activities}
+                    selectActivity={handleSelectActivity}
+                    selectedActivity={selectedActivity}
+                    editMode={editMode}
+                    setEditMode={setEditMode}
+                    setSelectedActivity={setSelectedActivity}
+                    createActivity={handleCreateActivity}
+                    editActivity={handleEditActivity}
+                    deleteActivity={handleDeleteActivity}
+                />
+            </Container>
+        </Fragment>
+    );
 };
 
 export default App;
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import { Menu, Container, Button } from 'semantic-ui-react';
+import React from 'react'
+import { Menu, Container, Button } from 'semantic-ui-react'
 
-const NavBar = () => {
-  return (
-    <Menu fixed="top" inverted>
-      <Container>
-        <Menu.Item header>
-          <img
-            src="/assets/logo.png"
-            alt="logo"
-            style={{ marginRight: '10px' }}
-          />
-          Reactivities
-        </Menu.Item>
-        <Menu.Item name="Activities" />
-        <Menu.Item>
-          <Button positive content="Create Activity" />
-        </Menu.Item>
-      </Container>
-    </Menu>
-  );
-};
+interface IProps {
+    openCreateForm: () => void
+}
 
-export default NavBar;
+const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
+    return (
+        <Menu fixed="top" inverted>
+            <Container>
+                <Menu.Item header>
+                    <img
+                        src="/assets/logo.png"
+                        alt="logo"
+                        style={{ marginRight: '10px' }}
+                    />
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name="Activities" />
+                <Menu.Item>
+                    <Button
+                        onClick={openCreateForm}
+                        positive
+                        content="Create Activity"
+                    />
+                </Menu.Item>
+            </Container>
+        </Menu>
+    )
+}
+
+export default NavBar
