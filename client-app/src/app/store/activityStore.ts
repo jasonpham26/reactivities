ff32afaf68ce6898578d32da3c5f8a4781df05f5
Jason Pham <vinhphuong0206@gmail.com>
2019-12-02 20:49:12 +1000

    Added Router and update ActivityStore

--- client-app/src/app/store/activityStore.ts
+++ client-app/src/app/store/activityStore.ts
@@ -6,10 +6,8 @@ import agent from '../api/agent';
 configure({ enforceActions: 'always' });
 export class ActivityStore {
     @observable activityRegistry = new Map();
-    @observable activities: IActivity[] = [];
-    @observable selectedActivity: IActivity | undefined;
+    @observable activity: IActivity | null = null;
     @observable loadingInitial = false;
-    @observable editMode = false;
     @observable submitting = false;
     @observable target = '';
 
@@ -39,8 +37,36 @@ export class ActivityStore {
     };
 
     @action selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-        this.editMode = false;
+        this.activity = this.activityRegistry.get(id);
+    };
+
+    @action loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if (activity) {
+            this.activity = activity;
+        } else {
+            this.loadingInitial = true;
+            try {
+                activity = await agent.Activities.details(id);
+                runInAction('getting activity', () => {
+                    this.activity = activity;
+                    this.loadingInitial = false;
+                });
+            } catch (error) {
+                runInAction('get activity error', () => {
+                    this.loadingInitial = false;
+                });
+                console.log(error);
+            }
+        }
+    };
+
+    @action clearActivity = () => {
+        this.activity = null;
+    };
+
+    getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
     };
 
     @action createActivity = async (activity: IActivity) => {
@@ -49,7 +75,6 @@ export class ActivityStore {
             await agent.Activities.create(activity);
             runInAction('creating activity', () => {
                 this.activityRegistry.set(activity.id, activity);
-                this.editMode = false;
                 this.submitting = false;
             });
         } catch (error) {
@@ -66,8 +91,7 @@ export class ActivityStore {
             await agent.Activities.update(activity);
             runInAction('editing activity', () => {
                 this.activityRegistry.set(activity.id, activity);
-                this.selectedActivity = activity;
-                this.editMode = false;
+                this.activity = activity;
                 this.submitting = false;
             });
         } catch (error) {
@@ -98,24 +122,6 @@ export class ActivityStore {
             });
             console.log(error);
         }
-    };
-
-    @action openCreateForm = () => {
-        this.editMode = true;
-        this.selectedActivity = undefined;
-    };
-
-    @action openEditForm = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-        this.editMode = true;
-    };
-
-    @action cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    };
-
-    @action cancelFormOpen = () => {
-        this.editMode = false;
     };
 }
 
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,27 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import ActivityList from './ActivityList';
-import ActivityDetails from '../details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/store/activityStore';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 const ActivityDashboard: React.FC = () => {
     const activityStore = useContext(ActivityStore);
-    const { editMode, selectedActivity } = activityStore;
+
+    useEffect(() => {
+        activityStore.loadActivities();
+    }, [activityStore]); // Second parameters to ensure this function does not get stuck in a loop - its also a dependencies array
+
+    if (activityStore.loadingInitial)
+        return <LoadingComponent content="Loading activities..." />;
     return (
         <Grid>
             <Grid.Column width={10}>
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width={6}>
-                {selectedActivity && !editMode && <ActivityDetails />}
-                {editMode && (
-                    <ActivityForm
-                        key={(selectedActivity && selectedActivity.id) || 0}
-                        activity={selectedActivity!}
-                    />
-                )}
+                <h2>Activity filters</h2>
             </Grid.Column>
         </Grid>
     );
